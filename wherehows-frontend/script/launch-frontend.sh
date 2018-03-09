3c8e2fcd0b0717dff099ca121505f359833d94c0
richardxin <richardxin168@yahoo.com>
2018-03-09 10:59:26 -0800

    move launch script to Wherehows

--- wherehows-frontend/script/launch-frontend.sh
+++ wherehows-frontend/script/launch-frontend.sh
@@ -9,8 +9,6 @@ set -a
 source ${EIDDO_DIR}/${CLOUD_APP}/application.env
 set +a
 
-cp ${EIDDO_DIR}/${CLOUD_APP}/bin/launch-frontend.sh ${APP_DIR}/bin/.
-
 #echo "creating symlink ..."
 #ln -sf ${EIDDO_DIR}/${CLOUD_APP}/conf/application.conf /opt/${CLOUD_APP}/conf/.
 export JAVA_OPTS="-Xms512m -Xmx2048m -Dhttp.port=9000"
