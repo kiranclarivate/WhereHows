9de6c7d939e9a1b4fdb8ca3d31500dee71543668
richardxin <richardxin168@yahoo.com>
2018-05-08 15:39:05 -0700

    test ingest script

--- wherehows-ingestion/script/upload_flow.sh
+++ wherehows-ingestion/script/upload_flow.sh
@@ -2,7 +2,7 @@
 
 usage(){
     echo "Usage:"
-    echo "$0 file_path url_and_port url_path"
+    echo "$0 file_type url_and_port url_path"
     echo "example:"
     echo "bin/upload_flow.sh json/example.json ec2-34-218-45-197.us-west-2.compute.amazonaws.com:8899 dataset"
     exit 1
@@ -15,11 +15,12 @@ fi
 
 echo "start uploading $1 to $2/$3"
 
-if [[ $1 = *"csv/"* ]]; then
-  echo "csv"
-elif [[ $1 = *"yaml/"* ]]; then
+if [[ $1 = "csv" ]]; then
+    echo "csv"
+elif [[ $1 = "yaml" ]]; then
     echo "yaml"
-elif [[ $1 = *"json/"* ]]; then
-    curl -H "Content-Type: application/json" -X POST -d "@$1" "$2/$3"
+elif [[ $1 = "json" ]]; then
+    for filename in json/*.json; do
+        curl -H "Content-Type: application/json" -X POST -d "@$filename" "$2/$3"
+    done
 fi
-
